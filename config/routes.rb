c1c39ee434f68cf53a3548be610e8b5080c58573
Cherub Kumar <cherub.kumar@gmail.com>
2015-05-11 19:31:00 -0500

    Modularized layouts and shit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'welcome/home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'triggers#index'
+  root 'welcome#home'
 
   match ':controller(/:action(/:id))', :via => :get
 
