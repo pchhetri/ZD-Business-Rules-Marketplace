9bc0a9687d55dc8b85af8ddbf947ad287e4852d6
Cherub Kumar <cherub.kumar@gmail.com>
2015-05-11 21:24:08 -0500

    Added bootstrap gem and made root to packages#index

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'zendesk_api'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.0'
+gem 'bootstrap-sass', '3.2.0.0'
 # Use postgresql as the database for Active Record
 gem 'pg'
 # Use SCSS for stylesheets
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'triggers#index'
+  root 'packages#index'
 
   match ':controller(/:action(/:id))', :via => [:get]
 
