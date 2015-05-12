7f39798754bc6690c4d9a4ccd66e2a34c72e361e
Cherub Kumar <cherub.kumar@gmail.com>
2015-05-12 03:54:20 -0500

    Create Zendesk Client from Session
    also creates confirm_logged_in method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,19 +8,33 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def confirm_logged_in
+    unless session[:username]
+      flash[:notice] = "Please enter your credentials and retry!"
+      redirect_to(:back)
+      return false # halts the before_action
+    else
+      return true
+    end
+  end
+
   def client
+    FooClient.instance(session[:subdomain], session[:username], session[:token])
   end
 
   class FooClient < ZendeskAPI::Client
+    def self.instance(subdomain=nil, username=nil, token=nil)
       @instance ||= new do |config|
 
+        config.url = "https://#{subdomain}.zendesk.com/api/v2"
+        config.username = username
+        config.token = token
 
         config.retry = true
 
         config.logger = Rails.logger
       end
     end
-
   end
 
 
